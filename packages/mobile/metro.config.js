a3f34c8f40ac3d5d8bd6142f6db763f8bb6886ae
Justin <mastfish@gmail.com>
2025-10-04 16:35:43 +1000

    wip on monorepoize

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -1,6 +1,14 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-
 const config = getDefaultConfig(__dirname);
-
 module.exports = withNativeWind(config, { input: "./global.css" });
+
+// const path = require("path");
+// const { getDefaultConfig } = require("expo/metro-config");
+// const { withNativeWind } = require("nativewind/metro");
+
+
+// // Add the CSS input option for NativeWind
+// module.exports = withNativeWind(config, {
+//   input: "./global.css",
+// });
