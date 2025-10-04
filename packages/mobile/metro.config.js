4e1ea391ebec86429381c3fe366e8d63617b5e73
Justin <mastfish@gmail.com>
2025-10-04 17:39:19 +1000

    fixup

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -1,24 +1,5 @@
-const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const { getDefaultConfig } = require("expo/metro-config");
+
 const config = getDefaultConfig(__dirname);
 module.exports = withNativeWind(config, { input: "./global.css" });
-
-
-// const { getDefaultConfig } = require('expo/metro-config');
-// const path = require('path');
-
-// const projectRoot = __dirname;
-// const monorepoRoot = path.resolve(projectRoot, '../..'); // adjust as needed
-
-// const config = getDefaultConfig(projectRoot);
-
-// // Watch for changes in the entire monorepo
-// config.watchFolders = [monorepoRoot];
-
-// // Resolve node modules both from project and monorepo root
-// config.resolver.nodeModulesPaths = [
-//   path.resolve(projectRoot, 'node_modules'),
-//   path.resolve(monorepoRoot, 'node_modules'),
-// ];
-
-// module.exports = config;
